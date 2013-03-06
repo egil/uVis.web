e1a5c5606c04dafedc035bd85fe9fe3ff3185cf7
Egil Hansen <egil@assimilated.dk>
2013-03-06 16:57:23 +0100

    Removing js and map files from repository

--- src/uvis/data/odata.js
+++
@@ -1,6 +0,0 @@
-//import dataModule = module('uvis/data');
-//import dm = dataModule.uvis.data;
-//export module uvis.data {
-//    // TODO: not yet implemented
-//}
-//@ sourceMappingURL=odata.js.map
