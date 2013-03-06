592f9661ed155d6dfdf8c2de4c4aa8b0adfc3038
Egil Hansen <egil@assimilated.dk>
2013-03-06 12:57:39 +0100

    merge with master

--- src/spec/component.spec.js
+++ src/spec/component.spec.js
@@ -1,22 +1,18 @@
-define(["require", "exports", 'uvis/property', 'uvis/component'], function(require, exports, __propertyModule__, __componentModule__) {
-    /// <reference path="../.typings/jasmine.d.ts" />
-    var propertyModule = __propertyModule__;
-
-    var componentModule = __componentModule__;
-
-    var up = propertyModule.uvis;
-    var uc = componentModule.uvis;
-    (function (uvis) {
-        (function (spec) {
-            describe('component:', function () {
-                describe('instantiating a new component', function () {
-                });
-                describe('creating content', function () {
-                });
+/// <reference path="../.typings/jasmine.d.ts" />
+var propertyModule = require("./uvis/property")
+var componentModule = require("./uvis/component")
+var up = propertyModule.uvis;
+var uc = componentModule.uvis;
+(function (uvis) {
+    (function (spec) {
+        describe('component:', function () {
+            describe('instantiating a new component', function () {
             });
-        })(uvis.spec || (uvis.spec = {}));
-        var spec = uvis.spec;
-    })(exports.uvis || (exports.uvis = {}));
-    var uvis = exports.uvis;
-})
+            describe('creating content', function () {
+            });
+        });
+    })(uvis.spec || (uvis.spec = {}));
+    var spec = uvis.spec;
+})(exports.uvis || (exports.uvis = {}));
+var uvis = exports.uvis;
 //@ sourceMappingURL=component.spec.js.map
--- src/spec/init.js
+++ src/spec/init.js
@@ -1,23 +1,21 @@
-define(["require", "exports"], function(require, exports) {
-    (function (uvis) {
-        /// <reference path="../.typings/require.d.ts" />
-        (function (spec) {
-            function init(jasmineEnv) {
-                require([
-                    'spec/util.promise.spec', 
-                    'spec/util.dictionary.spec', 
-                    'spec/property.spec', 
-                    'spec/component.spec'
-                ], function () {
-                    jasmineEnv.execute();
-                }, function (err) {
-                    console.error('Unable to load some or all of the requires specs. Error message = ' + err);
-                });
-            }
-            spec.init = init;
-        })(uvis.spec || (uvis.spec = {}));
-        var spec = uvis.spec;
-    })(exports.uvis || (exports.uvis = {}));
-    var uvis = exports.uvis;
-})
+(function (uvis) {
+    /// <reference path="../.typings/require.d.ts" />
+    (function (spec) {
+        function init(jasmineEnv) {
+            require([
+                'spec/util.promise.spec', 
+                'spec/util.dictionary.spec', 
+                'spec/property.spec', 
+                'spec/component.spec'
+            ], function () {
+                jasmineEnv.execute();
+            }, function (err) {
+                console.error('Unable to load some or all of the requires specs. Error message = ' + err);
+            });
+        }
+        spec.init = init;
+    })(uvis.spec || (uvis.spec = {}));
+    var spec = uvis.spec;
+})(exports.uvis || (exports.uvis = {}));
+var uvis = exports.uvis;
 //@ sourceMappingURL=init.js.map
